3c755c0a9d53cd1b76ca4d190d50d9abfb6b05f5
Jason <jason741852@gmail.com>
2017-03-11 12:45:14 -0800

    Cange startup activity for databse testing purposes

---
+++ app/src/main/java/com/example/tyson/freeps/PostDetails.java
@@ -0,0 +1,17 @@
+package com.example.tyson.freeps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class PostDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_details);
+
+    }
+}
