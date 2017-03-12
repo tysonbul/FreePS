aa271e3c47f696e646d4148da804a3a83c6332a0
soxies1 <tyson_bulmer@hotmail.com>
2017-03-11 17:07:14 -0800

    Add map, splash, FindOrPost activities, and basic dialog function

---
+++ app/src/main/java/com/example/tyson/freeps/FindOrPostActivity.java
@@ -0,0 +1,13 @@
+package com.example.tyson.freeps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindOrPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_or_post);
+    }
+}
