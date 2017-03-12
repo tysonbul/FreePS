2eda18e56e40f6e36119b1cddfe05722c0558a4e
soxies1 <tyson_bulmer@hotmail.com>
2017-03-12 01:16:10 -0800

    Add post creation activity and new buttons

---
+++ app/src/main/java/com/example/tyson/freeps/AddPostActivity.java
@@ -0,0 +1,26 @@
+package com.example.tyson.freeps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class AddPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+        ImageButton postButton = (ImageButton) findViewById(R.id.post_or_cancel_button);
+        postButton.setImageResource(R.drawable.x_cancel);
+
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
